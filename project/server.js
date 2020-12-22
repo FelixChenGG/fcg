848c50f8d341cc96a9ba190430aa40fe38d391f2
FelixChenGG <75570618+FelixChenGG@users.noreply.github.com>
2020-12-22 12:05:13 +0800

    Update server.js

--- project/server.js
+++ project/server.js
@@ -334,4 +334,28 @@ app.get('/logout', (req,res) => {
 	res.redirect('/');
 });
 
+app.get('/api/restaurants/names/:name', (req,res) => {
+    if (req.params.name) {
+        let criteria = {};
+        criteria['name'] = req.params.name;
+        const client = new MongoClient(mongourl);
+        client.connect((err) => {
+            assert.equal(null, err);
+            console.log("Connected successfully to server");
+            const db = client.db(dbName);
+
+            findRestaurant(db, criteria, (docs) => {
+                client.close();
+				console.log("Closed DB connection");
+				
+					res.status(200).json(docs);
+				
+                
+            });
+        });
+    } else {
+        res.status(500).json({"error": "missing bookingid"});
+    }
+})
+
 app.listen(process.env.PORT || 8099);
